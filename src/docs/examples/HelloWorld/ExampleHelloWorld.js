275d025f10c250eb5ff4503926d24380e646e0e4
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-06-30 03:47:23 -0400

    adding the docs applicaiton files and first example component

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'rrc/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Developers!" />
+}
